63ae8cb0aef1eb20ddedca30df23fa0d82511e6d
BalbhadraDaxini <balbhadraiitb@gmail.com>
2026-07-02 23:00:56 +0530

    Commit

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   apiUrl: '/api/v1',
-  razorpayKeyId: 'rzp_test_STo8X60gyG9PvZ',
+  razorpayKeyId: 'rzp_live_T8byvlnWZiQsH3',
   msg91: {
     widgetId: '366761734467323939313830',
     tokenAuth: '545115THqFCivXp6a456ae2P1',
